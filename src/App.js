e4c1a2fea3a9536b527fd931968586b5df5467f6
stephen <swcollins462@gmail.com>
2023-07-13 18:54:27 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import { useReducer } from "react";
 import './App.css';
 
+const initialState = {
+  currentOperand: "0",
+  previousOperand: null,
+  operation: null,
+  memory: null,
+  overwrite: true
+};
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [{ currentOperand, previousOperand, operation, memory }, dispatch] = useReducer(
+    reducer,
+    initialState
   );
-}
+  return (
+    <div className="calculator-grid">
+      <NumericDisplay 
+        previousOperand={previousOperand}
+        currentOperand={currentOperand}
+        operation={operation}
+        memory={memory}
+        formatOperand={formatOperand}
+      />
+      <CalculatorButtons dispatch={dispatch} currentOperand={currentOperand} />
+    </div>
+  )
+};
 
 export default App;
