49c7adddbb127230b3e6e74d4d9381dc710abb3e
stephen <swcollins462@gmail.com>
2023-07-13 19:36:10 -0400

    Reducer function

--- src/App.js
+++ src/App.js
@@ -16,6 +16,108 @@ export const ACTIONS = {
   EVALUATE: 'evaluate'
 };
 
+function reducer(state, { type, payload }) {
+  switch(type) {
+    case ACTIONS.ADD_DIGIT:
+      if (state.currentOperand === "0" && payload.digit === "0") return state;
+      if (state.overwrite) {
+        return {
+          ...state,
+          currentOperand: payload.digit,
+          overwrite: false,
+        };
+      } else if (
+        payload.digit === "." && 
+        state.currentOperand.includes(".")) return state;
+      if (state.currentOperand.length < 15) {
+        return {
+        ...state,
+        currentOperand: `${state.currentOperand}${payload.digit}`
+        };
+      };
+      return state;
+    case ACTIONS.CHOOSE_OPERATION: 
+      if (state.currentOperand === "." || state.currentOperand === "0.") return state;
+      if (state.currentOperand === "0" && state.previousOperand == null) return state;
+
+      if (state.currentOperand === "0") {
+        return {
+          ...state,
+          operation: payload.operation,
+        };
+      } else if (state.previousOperand == null) {
+        return {
+          ...state,
+          operation: payload.operation,
+          previousOperand: state.currentOperand,
+          currentOperand: "0",
+        };
+      };
+      return {
+        ...state,
+        previousOperand: evaluate(state),
+        operation: payload.operation,
+        currentOperand: "0"
+      };
+    case ACTIONS.CLEAR:
+      if (state.currentOperand !== "0") {
+        return {
+          ...state,
+          currentOperand: "0"
+        };
+      };
+      return {
+        ...state,
+        previousOperand: null,
+        operation: null,
+        currentOperand: "0"
+      };
+    case ACTIONS.NEGATE:
+      if (state.currentOperand === "." || state.currentOperand === "0.") return state;
+      if (state.currentOperand !== "0") {
+        return {
+          ...state,
+          currentOperand: state.currentOperand
+        };
+      };
+      return state;
+    case ACTIONS.PERCENT:
+      if (state.currentOperand === "." || state.currentOperand === "0.") return state;
+      if (state.operation === "+" || state.operation === "−") {
+        return {
+
+        };
+      } else if (state.currentOperand !== "0") {
+        return {
+          
+        };
+      };
+      return state;
+    case ACTIONS.EVALUATE: 
+      if (
+        state.operation == null ||
+        state.previousOperand == null
+      ) {
+        return state;
+      };
+      return {
+        ...state,
+        overwrite: true,
+        previousOperand: null,
+        operation: null,
+        currentOperand: evaluate(state)
+      };
+    default:
+      return state;
+  }
+};
+
+function evaluate({ currentOperand, previousOperand, operation}) {
+  let computation = ""
+
+  return computation.toString()
+};
+
 const initialState = {
   currentOperand: "0",
   previousOperand: null,
